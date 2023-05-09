7d6ff388559c78e2b0e73100088340f3e3321e4b
Anthony Romaine <romaine@unlv.nevada.edu>
2023-05-08 18:32:14 -0700

    Refactor page logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,18 @@
-import LL from "./components/LL/LL";
-import First from "./components/First/First";
 import NavLayout from "./components/NavLayout/NavLayout";
 import { AppStore } from "./state";
-import { pages } from "./types";
-import LR0 from "./components/LR0/LR0";
-import LR0DFA from "./components/LR0DFA/LR0DFA";
-import SLR from "./components/SLR/SLR";
-import LR1 from "./components/LR1/LR1";
+import { pageComponents } from "./types";
 
 export default function App() {
   const page = AppStore((state) => state.page);
 
-  if (page === pages.FIRST) {
+  const Page = pageComponents.get(page);
+  if (Page){
     return (
       <NavLayout>
-        <First />
+        <Page />
       </NavLayout>
-    );
-  } else if (page === pages.LL) {
-    return (
-      <NavLayout>
-        <LL />
-      </NavLayout>
-    );
-  } else if (page === pages.LR0) {
-    return (
-      <NavLayout>
-        <LR0 />
-      </NavLayout>
-    );
-  } else if (page === pages.LR0DFA) {
-    return (
-      <NavLayout>
-        <LR0DFA />
-      </NavLayout>
-    );
-  } else if (page === pages.SLR) {
-    return (
-      <NavLayout>
-        <SLR />
-      </NavLayout>
-    );
-  } else if (page === pages.LR1) {
-    return (
-      <NavLayout>
-        <LR1 />
-      </NavLayout>
-    );
+    )
   } else {
-    return <NavLayout />;
+    return <NavLayout />
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,11 @@
+import First from "./components/First/First";
+import LL from "./components/LL/LL";
+import LR0 from "./components/LR0/LR0";
+import LR0DFA from "./components/LR0DFA/LR0DFA";
+import LR1 from "./components/LR1/LR1";
+import SLR from "./components/SLR/SLR";
+import { LALR1Graph } from "./models/LALR1Graph";
+
 export const pages = {
   FIRST: "First, Follow, and Predict Sets",
   LL: "LL(1) Parse Table",
@@ -7,6 +15,15 @@ export const pages = {
   LR1: "LR(1) Automaton",
   LALR: "LALR(1) Automaton",
 } as const;
+
+export const pageComponents: Map<string, () => JSX.Element> = new Map([
+  [pages.FIRST, First],
+  [pages.LL, LL],
+  [pages.LR0, LR0],
+  [pages.LR0DFA, LR0DFA],
+  [pages.SLR, SLR],
+  [pages.LR1, LR1],
+]);
 
 export type GRAMMAR_INPUT = {
   id: string;
